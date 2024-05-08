b1489dd1299853d08da012e79b55e7b4dffe6cc4
S07C08 <S07C08@S07C08>
2024-05-08 08:48:15 +0330

    Add project files.

---
+++ alireza majd/Program.cs
@@ -0,0 +1,11 @@
+﻿//Alireza Keshavarz Mowlaei
+//Ali majd
+
+Console.Write("Enter the first Number:");
+int FirstNumber = Convert.ToInt32(Console.ReadLine());
+
+Console.Write("Enter the Second Number:");
+int SecondNumber = Convert.ToInt32(Console.ReadLine());
+
+int Sum = FirstNumber + SecondNumber;
+Console.WriteLine($"the Sum of {FirstNumber} and {SecondNumber} is {Sum}");
